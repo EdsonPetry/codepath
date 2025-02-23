8d070a73956d50bacfe6cf176bc3abae236c47bd
Edson Petry <edsonlbpetry@gmail.com>
2025-02-23 17:56:19 -0500

    Completed session 1, standard v1 problem 2

---
+++ tip102/01-strings-arrays/session-1/standard-v1/problem-2.py
@@ -0,0 +1,9 @@
+def main():
+    greetings("Michael")
+    greetings("Winnie the Pooh")
+
+def greetings(name: str) -> None:
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Crhistopher Robin.")
+
+if __name__ == "__main__":
+    main()
