3d2bbb10757d9cc2dee3046d36a94c215e17cddf
Edson Petry <edsonlbpetry@gmail.com>
2025-02-23 19:13:39 -0500

    Completed session 1, standard v1, problem 9

--- tip102/01-strings-arrays/session-1/standard-v1/problem-9.py
+++ tip102/01-strings-arrays/session-1/standard-v1/problem-9.py
@@ -1,15 +1,15 @@
 def main():
     item_quantities = [2, 4, 6, 8]
-    can_pair(item_quantities)
+    print(can_pair(item_quantities))
 
     item_quantities = [1, 2, 3, 4]
-    can_pair(item_quantities)
+    print(can_pair(item_quantities))
 
     item_quantities = []
-    can_pair(item_quantities)
+    print(can_pair(item_quantities))
 
-def can_pair(item_quanitities):
-    return len(item_quanitities) % 2 == 0
+def can_pair(item_quanitities: list) -> bool:
+    return all(item % 2 == 0 for item in item_quanitities)
 
 if __name__ == "__main__":
     main()
