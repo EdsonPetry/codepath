d7615921eea0e7d172929bb3c772a87fea5bfdda
EdsonPetry <edsonlbpetry@gmail.com>
2025-03-18 19:54:47 -0400

    Initialized problems for standard version 1

---
+++ tip102/07-recursions-divide-n-conquer/session-1/problem-1.py
@@ -0,0 +1,31 @@
+"""
+Problem 1: Counting Iron Man's Suits
+Tony Stark, aka Iron Man, has designed many different suits over the years. Given a list
+of strings suits where each string is a suit in Stark's collection, count the total number
+ of suits in the list.
+
+Implement the solution iteratively without the use of the len() function.
+Implement the solution recursively.
+Discuss: what are the similarities between the two solutions? What are the differences?
+
+Example Output:
+3
+4
+"""
+
+
+def count_suits_iterative(suits):
+    pass
+
+
+def count_suits_recursive(suits):
+    pass
+
+
+def main():
+    print(count_suits_iterative(["Mark I", "Mark II", "Mark III"]))
+    print(count_suits_recursive(["Mark I", "Mark I", "Mark III", "Mark IV"]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tip102/07-recursions-divide-n-conquer/session-1/problem-6.py
@@ -0,0 +1,30 @@
+"""
+Problem 6: Strongest Avenger
+The Avengers need to determine who is the strongest. Given a list of their
+strengths, find the maximum strength using a recursive approach without using
+the max() function.
+
+Evaluate the time complexity of your solution. Define your variables and provide
+a rationale for why you believe your solution has the stated time complexity.
+
+Example Output:
+
+100
+Example 1 Explanation: The maximum strength among the Avengers is 100.
+
+90
+Example 2 Explanation: The maximum strength among the Avengers is 90.
+"""
+
+
+def strongest_avenger(strengths):
+    pass
+
+
+def main():
+    print(strongest_avenger([88, 92, 95, 99, 97, 100, 94]))
+    print(strongest_avenger([50, 75, 85, 60, 90]))
+
+
+if __name__ == "__main__":
+    main()
